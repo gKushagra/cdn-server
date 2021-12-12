c64948616d57cc983b68cc4f31459f8586781573
gKushagra <55290773+gKushagra@users.noreply.github.com>
2021-12-11 23:03:55 -0600

    commented un wanted code

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var express = require('express');
 var formidable = require('formidable');
 var path = require('path');
 var fs = require('fs');
-var mime = require('mime-types');
+// var mime = require('mime-types');
 var router = express.Router();
 var cStorageDir = path.resolve(__dirname + process.env.C_STORAGE);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.status(200).send('Welcome to (D:) Drive File API');
+  res.status(200).send('Hello! This is Open File Server!');
 });
 
 /** POST upload a file */
@@ -89,20 +89,20 @@ router.post('/file/download', function (req, res, next) {
 });
 
 /** POST rename a file */
-router.post('/file/rename', function (req, res, next) {
-  let data = req.body;
-  // console.log(data);
-  try {
-    let currDirArr = data.filePath.split("/");
-    let currDirPath = currDirArr.slice(0, currDirArr.length - 1).toString();
-    let newFilePath = currDirPath.replace(/,/g, "/") + `/${data.newName}`;
-    fs.renameSync(data.filePath, newFilePath);
-    res.status(200).json("renamed");
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
-});
+// router.post('/file/rename', function (req, res, next) {
+//   let data = req.body;
+//   // console.log(data);
+//   try {
+//     let currDirArr = data.filePath.split("/");
+//     let currDirPath = currDirArr.slice(0, currDirArr.length - 1).toString();
+//     let newFilePath = currDirPath.replace(/,/g, "/") + `/${data.newName}`;
+//     fs.renameSync(data.filePath, newFilePath);
+//     res.status(200).json("renamed");
+//   } catch (err) {
+//     console.error(err);
+//     res.sendStatus(500);
+//   }
+// });
 
 /** POST delete a file */
 router.post('/file/delete', function (req, res, next) {
@@ -118,95 +118,95 @@ router.post('/file/delete', function (req, res, next) {
 });
 
 /** GET directory tree */
-router.get('/directory/:id', function (req, res, next) {
-  let rootDir = `${cStorageDir}/${req.params.id}`;
-  try {
-    if (!fs.existsSync(rootDir)) {
-      fs.mkdirSync(rootDir)
-    }
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
+// router.get('/directory/:id', function (req, res, next) {
+//   let rootDir = `${cStorageDir}/${req.params.id}`;
+//   try {
+//     if (!fs.existsSync(rootDir)) {
+//       fs.mkdirSync(rootDir)
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     res.sendStatus(500);
+//   }
 
-  let root = new TreeNode(rootDir);
-  let stack = [root];
+//   let root = new TreeNode(rootDir);
+//   let stack = [root];
 
-  while (stack.length) {
-    let currentNode = stack.pop();
+//   while (stack.length) {
+//     let currentNode = stack.pop();
 
-    if (currentNode) {
-      let children = fs.readdirSync(currentNode.path);
+//     if (currentNode) {
+//       let children = fs.readdirSync(currentNode.path);
 
-      for (let child of children) {
-        let childPath = `${currentNode.path}/${child}`;
-        let file = {};
-        file.name = path.basename(childPath);
-        file.type = mime.lookup(childPath);
-        file.lastModified = fs.statSync(childPath).mtime;
-        file.size = fs.statSync(childPath).size;
-        let childNode = new TreeNode(childPath, file);
-        currentNode.children.push(childNode);
+//       for (let child of children) {
+//         let childPath = `${currentNode.path}/${child}`;
+//         let file = {};
+//         file.name = path.basename(childPath);
+//         file.type = mime.lookup(childPath);
+//         file.lastModified = fs.statSync(childPath).mtime;
+//         file.size = fs.statSync(childPath).size;
+//         let childNode = new TreeNode(childPath, file);
+//         currentNode.children.push(childNode);
 
-        if (fs.statSync(childNode.path).isDirectory()) {
-          stack.push(childNode);
-        }
-      }
-    }
-  }
+//         if (fs.statSync(childNode.path).isDirectory()) {
+//           stack.push(childNode);
+//         }
+//       }
+//     }
+//   }
 
-  res.status(200).json(root);
-});
+//   res.status(200).json(root);
+// });
 
 /** POST add a directory */
-router.post('/directory/add', function (req, res, next) {
-  let data = req.body;
-  // console.log(data);
-  try {
-    if (!fs.existsSync(data.newDirPath)) {
-      fs.mkdirSync(data.newDirPath)
-      res.status(200).json("created");
-    } else {
-      res.status(200).json("directory-exists");
-    }
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
-});
+// router.post('/directory/add', function (req, res, next) {
+//   let data = req.body;
+//   // console.log(data);
+//   try {
+//     if (!fs.existsSync(data.newDirPath)) {
+//       fs.mkdirSync(data.newDirPath)
+//       res.status(200).json("created");
+//     } else {
+//       res.status(200).json("directory-exists");
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     res.sendStatus(500);
+//   }
+// });
 
 /** POST rename a directory */
-router.post('/directory/rename', function (req, res, next) {
-  let data = req.body;
-  // console.log(data);
-  try {
-    let currDirArr = data.dirPath.split("/");
-    let currDirPath = currDirArr.slice(0, currDirArr.length - 1).toString();
-    let newDirPath = currDirPath.replace(/,/g, "/") + `/${data.newName}`;
-    if (!fs.existsSync(newDirPath)) {
-      fs.renameSync(data.dirPath, newDirPath);
-      res.status(200).json("renamed");
-    } else {
-      res.status(200).json("directory-name-exists");
-    }
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
-});
+// router.post('/directory/rename', function (req, res, next) {
+//   let data = req.body;
+//   // console.log(data);
+//   try {
+//     let currDirArr = data.dirPath.split("/");
+//     let currDirPath = currDirArr.slice(0, currDirArr.length - 1).toString();
+//     let newDirPath = currDirPath.replace(/,/g, "/") + `/${data.newName}`;
+//     if (!fs.existsSync(newDirPath)) {
+//       fs.renameSync(data.dirPath, newDirPath);
+//       res.status(200).json("renamed");
+//     } else {
+//       res.status(200).json("directory-name-exists");
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     res.sendStatus(500);
+//   }
+// });
 
 /** POST delete a directory */
-router.post('/directory/delete', function (req, res, next) {
-  let data = req.body;
-  // console.log(data);
-  try {
-    fs.rmdirSync(data.dirPath, { recursive: true });
-    res.status(200).json("deleted");
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
-});
+// router.post('/directory/delete', function (req, res, next) {
+//   let data = req.body;
+//   // console.log(data);
+//   try {
+//     fs.rmdirSync(data.dirPath, { recursive: true });
+//     res.status(200).json("deleted");
+//   } catch (err) {
+//     console.error(err);
+//     res.sendStatus(500);
+//   }
+// });
 
 module.exports = router;
 
