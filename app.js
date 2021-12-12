27603ee1b0dc82f701d5de5e4cbf5d8367547fc9
gKushagra <55290773+gKushagra@users.noreply.github.com>
2021-12-11 23:03:28 -0600

    minimized server code

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
+require('dotenv').config();
 var express = require('express');
+var http = require('http');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var cors = require('cors');
-
 var indexRouter = require('./routes/index');
+var port = process.env.PORT;
 
 var app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/', indexRouter);
 
-module.exports = app;
+var server = http.createServer(app);
+server.listen(port, () => {
+    console.log(`file server running on port ${port}`)
+});
