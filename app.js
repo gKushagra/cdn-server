4f330377a14a8d23a3f418789c81c43f5b5e6c9f
Kushagra Gupta <dev.gkushagra@gmail.com>
2023-03-05 23:32:23 -0500

    added upload download and copy to clipboard

--- app.js
+++ app.js
@@ -5,92 +5,78 @@ var path = require('path');
 var cors = require('cors');
 var formidable = require('formidable');
 var fs = require('fs');
-var indexRouter = require('./routes/index');
+var axios = require('axios').default;
+var mime = require('mime-types');
 
 var port = process.env.PORT;
-var cStorageDir = path.resolve(__dirname + process.env.C_STORAGE);
+var rootDir = path.resolve(__dirname + process.env.FILE_DIR);
+var redirectUri = process.env.REDIRECT_URI;
+var singleSignOn = process.env.SINGLE_SIGN_ON;
 var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'www')));
 app.use(express.static(path.join(__dirname, 'files')));
 app.use(cors());
 
+function checkAuth(req, res, next) {
+    const token = req.headers.authorization;
+    if (token && token !== '') {
+        axios.post(singleSignOn + '/verify?redirectUri=' + redirectUri)
+            .then(response => {
+                if (response.status === 401) { res.status(401).json("Unauthorized"); }
+                else { next(); }
+            })
+            .catch(err => res.sendStatus(500));
+    } else {
+        res.status(401).json("Unauthorized");
+    }
+}
+
+if (!fs.existsSync(rootDir)) {
+    fs.mkdirSync(rootDir);
+}
+
 /* GET home page. */
 app.get('/', function (req, res, next) {
-    res.status(200).send('Hello! This is Open File Server!');
+    res.sendFile(path.join(__dirname, 'www', 'index.html'));
+});
+
+app.get('/portal', function (req, res, next) {
+    res.sendFile(path.join(__dirname, 'www', 'portal.html'));
 });
 
 /** POST upload a file */
-app.post('/file/upload/:id/:dir?', function (req, res, next) {
-    let rootDir = `${cStorageDir}/${req.params.id}`;
-    // check if root exists
-    try {
-        if (!fs.existsSync(rootDir)) {
-            fs.mkdirSync(rootDir)
-        }
-    } catch (err) {
-        console.error(err);
-        res.sendStatus(500);
-    }
-    let dirPath = rootDir;
-    let searchDir = req.params.dir;
-    // if there is specific upload directory, get path
-    if (searchDir !== "null") {
-        let root = new TreeNode(rootDir);
-        let stack = [root];
-
-        while (stack.length) {
-            let currentNode = stack.pop();
-
-            if (currentNode) {
-                let children = fs.readdirSync(currentNode.path);
-
-                for (let child of children) {
-                    let childPath = `${currentNode.path}/${child}`;
-                    if (child === searchDir) dirPath = childPath;
-                    let file = {};
-                    let childNode = new TreeNode(childPath, file);
-                    currentNode.children.push(childNode);
-
-                    if (fs.statSync(childNode.path).isDirectory()) {
-                        stack.push(childNode);
-                    }
-                }
-                if (dirPath !== rootDir)
-                    break;
-            }
-        }
-    }
+app.post('/files', function (req, res, next) {
     // formidable options
     let options = {
-        uploadDir: dirPath,
+        uploadDir: rootDir,
         keepExtensions: true,
         maxFileSize: 1024 * 1024 * 1024,
         allowEmptyFiles: true,
-        multiples: true,
+        multiples: false,
     }
     // save file
     let form = new formidable.IncomingForm(options);
     form.on('file', function (field, file) {
         //rename the incoming file to the file's name
-        fs.renameSync(file.path, dirPath + "/" + file.name);
+        fs.renameSync(file.path, rootDir + "/" + file.name);
     });
     form.parse(req, (err, fields, files) => {
         if (err) {
             console.error(err);
-            res.sendStatus(500);
+            return res.sendStatus(500);
         }
         res.json({ fields, files });
     });
 });
 
 /** POST download a file */
-app.post('/file/download', function (req, res, next) {
-    let data = req.body;
-    // console.log(data);
+app.get('/files/:filename', function (req, res, next) {
+    let filePath = `${rootDir}/${req.params.filename}`;
     try {
-        res.download(data.filePath);
+        res.download(filePath);
     } catch (err) {
         console.error(err);
         res.sendStatus(500);
@@ -98,16 +84,44 @@ app.post('/file/download', function (req, res, next) {
 });
 
 /** POST delete a file */
-app.post('/file/delete', function (req, res, next) {
-    let data = req.body;
-    // console.log(data);
+app.delete('/files/:filename', function (req, res, next) {
+    let filePath = `${rootDir}/${req.params.filename}`;
     try {
-        fs.unlinkSync(data.filePath);
+        fs.unlinkSync(filePath);
         res.status(200).json("deleted");
     } catch (err) {
         console.error(err);
         res.sendStatus(500);
     }
+});
+
+/** GET directory tree */
+app.get('/directory', function (req, res, next) {
+    let root = new TreeNode(rootDir);
+    let stack = [root];
+    while (stack.length) {
+        let currentNode = stack.pop();
+
+        if (currentNode) {
+            let children = fs.readdirSync(currentNode.path);
+
+            for (let child of children) {
+                let childPath = `${currentNode.path}/${child}`;
+                let file = {};
+                file.name = path.basename(childPath);
+                file.type = mime.lookup(childPath);
+                file.lastModified = fs.statSync(childPath).mtime;
+                file.size = fs.statSync(childPath).size;
+                let childNode = new TreeNode(childPath, file);
+                currentNode.children.push(childNode);
+
+                if (fs.statSync(childNode.path).isDirectory()) {
+                    stack.push(childNode);
+                }
+            }
+        }
+    }
+    res.status(200).json(root);
 });
 
 var server = http.createServer(app);
